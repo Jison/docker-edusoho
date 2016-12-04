c3ae237132fa158e03271611d023768a7de99c44
Simon <wq@zlzkj.com>
2016-12-05 01:35:03 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 #set -eo pipefail
 
-#extract edusoho
 hasLock=
 if [ -f "/var/www/entrypoint-executed.lock" ]; then
     hasLock=true
@@ -11,7 +10,8 @@ else
 fi
 
 if [ !hasLock ]; then
-    tar zxvf /var/www/edusoho-${EDUSOHO_VERSION}.tar.gz -C /var/www && rm -rf /var/www/edusoho-${EDUSOHO_VERSION}.tar.gz
+    #extract edusoho
+    tar zxvf /var/www/edusoho-${EDUSOHO_VERSION}.tar.gz -C /var/www && chown -R www-data:www-data edusoho && rm -rf /var/www/edusoho-${EDUSOHO_VERSION}.tar.gz
     touch /var/www/entrypoint-executed.lock
 
 
@@ -21,10 +21,10 @@ if [ !hasLock ]; then
         exit 1
     fi
     sed -i "s/{{DOMAIN}}/${DOMAIN}/g" /etc/nginx/sites-enabled/edusoho.conf
-fi
 
-#init datadir if mount dir outside to /var/lib/mysql
-#mysql_install_db
+    #init datadir if mount dir outside to /var/lib/mysql
+    mysql_install_db
+fi
 
 #start services
 echo 'starting nginx'
@@ -32,10 +32,10 @@ echo 'starting nginx'
 echo 'nginx is running'
 
 echo 'starting php5-fpm'
-/etc/init.d/php5-fpm start
+/etc/init.d/php5-fpm start >& /dev/null
 echo 'php5-fpm is running'
 
-echo "starting mysql \c"
+echo "starting mysql"
 /etc/init.d/mysql start
 mysql_root='mysql -uroot'
 # echo 'SELECT 1' | ${mysql_root} &> /dev/null
@@ -43,11 +43,11 @@ mysql_root='mysql -uroot'
 #     echo >&2 'mysql start failed.'
 #     exit 1
 # fi
-for i in {10..0}; do
-    if echo 'SELECT 1' | ${mysql_root} &> /dev/null; then
+for i in {30..0}; do
+    if echo 'SELECT 1' | ${mysql_root} >& /dev/null; then
         break
     fi
-    echo "# \c"
+    echo "waiting..."
     sleep 1
 done
 
@@ -62,6 +62,10 @@ else
         echo 'CREATE DATABASE IF NOT EXISTS `edusoho` DEFAULT CHARACTER SET utf8 ;' | ${mysql_root}
         echo 'GRANT ALL PRIVILEGES ON `edusoho`.* TO "esuser"@"localhost" IDENTIFIED BY "edusoho";' | ${mysql_root}
         echo 'create database for edusoho successfully'
+
+        mysqladmin -uroot password root
+        sed -i "s/user\s*=\s*debian-sys-maint/user = root/g" /etc/mysql/debian.cnf
+        sed -i "s/password\s*=\s*\w*/password = root/g" /etc/mysql/debian.cnf
     fi
     echo '***************************'
     echo '* welcome to use edusoho! *'
